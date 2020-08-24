1b48fc48c7fb127b109ecff7dc6e1aed0bc29e9b
Prasun Thapliyal <prasun.thapliyal@gmail.com>
2020-08-24 16:03:08 +0530

    Reproduce Object-Deleted exception

--- OnepMini/OnepMini/Controllers/NetworksController.cs
+++ OnepMini/OnepMini/Controllers/NetworksController.cs
@@ -52,6 +52,8 @@ namespace OnepMini.Controllers
 
                     tableEntries.Add($"Row Count = {rowCount}");
                     tx.Commit();
+                    session.Close();
+
                 }
                 catch (Exception ex)
                 {
@@ -59,6 +61,8 @@ namespace OnepMini.Controllers
 
                     Debug.WriteLine(ex.ToString());
                     tx.Rollback();
+                    session.Close();
+
                     throw;
                 }
             }
@@ -87,6 +91,8 @@ namespace OnepMini.Controllers
                 {
                     await session.SaveOrUpdateAsync(onepNetwork).ConfigureAwait(false);
                     tx.Commit();
+                    session.Close();
+
                 }
                 catch (Exception ex)
                 {
@@ -94,6 +100,8 @@ namespace OnepMini.Controllers
 
                     Debug.WriteLine(ex.ToString());
                     tx.Rollback();
+                    session.Close();
+
                     throw;
                 }
             }
@@ -205,9 +213,244 @@ namespace OnepMini.Controllers
                 onepValidationResult01.OnepValidochpaths.Add(onepVP01_2);
                 onepNetwork.OnepValidationresults.Add(onepValidationResult01);
 
+                // Create another Validation result
+
+                var onepValidationResult02 = new OnepValidationresult
+                {
+                    Status = 1,
+                    OnepNetwork = onepNetwork
+                };
+                onepNetwork.OnepValidationresults.Add(onepValidationResult02);
+
             }
 
             return onepNetwork;
+        }
+
+        // POST: api/Networks
+        [HttpPost("CreateNetworkForLEReset_01")]
+        public async Task<object> CreateNetworkForLEReset_01([FromBody] string value)
+        {
+            // Create a basic Onep Network and save to DB
+            OnepNetwork onepNetwork = CreateNetworkForLEReset_01();
+
+            var session = _sessionFactory.OpenSession();
+            using (var tx = session.BeginTransaction())
+            {
+                try
+                {
+                    await session.SaveOrUpdateAsync(onepNetwork).ConfigureAwait(false);
+                    tx.Commit();
+                    session.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Debugger.Break();
+
+                    Debug.WriteLine(ex.ToString());
+                    tx.Rollback();
+                    session.Close();
+
+                    throw;
+                }
+            }
+            return new { Id = onepNetwork.Id };
+        }
+
+        [HttpPut("EditNetwork/{id}/ResetLE_01")]
+        public async Task<object> ResetLE_01(long id, [FromBody] string value)
+        {
+            // This sequence of changes results in the following exception:
+            // NHibernate.ObjectDeletedException: deleted object would be re-saved by cascade (remove deleted object from associations)[OnepMini.OrmNhib.BusinessObjects.OnepValidochpath#163840]
+
+            // Summary:
+            // We already have VR01 and VR02 in the network
+            // During LE reset, move VPs from under VR01 to VR02
+            // And then we remove VR01 from network
+
+            // Get the network
+            var session = _sessionFactory.OpenSession();
+            var onepNetwork = session.Get<OnepNetwork>(id);
+
+            // LE Reset
+            // Get ValidationResult01's VP's and associate them with a new ValidationResult
+            ResetLE_01(onepNetwork);
+
+            // Finally save the network back
+            using (var tx = session.BeginTransaction())
+            {
+                try
+                {
+                    await session.SaveOrUpdateAsync(onepNetwork).ConfigureAwait(false);
+                    tx.Commit();
+                    session.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Debugger.Break();
+
+                    Debug.WriteLine(ex.ToString());
+                    tx.Rollback();
+                    session.Close();
+                    throw;
+                }
+            }
+
+            return new { Id = id };
+
+        }
+
+        private static OnepNetwork CreateNetworkForLEReset_01()
+        {
+            // This network and its corresponding LE Reset is causing
+            // Deleted object would be re-saved exception
+
+            OnepNetwork onepNetwork;
+            {
+                onepNetwork = new OnepNetwork()
+                {
+                    McpProjectId = Guid.NewGuid().ToString(),
+                    Name = "Test 01"
+                };
+                var onepFiberTL1 = new OnepFibertl()
+                {
+                    Name = "FS01_1",
+                    Length = 30,
+                    Loss = 30 * 0.25,
+                    OnepNetwork = onepNetwork
+                };
+                var onepFiberTL2 = new OnepFibertl()
+                {
+                    Name = "FS01_2",
+                    Length = 30,
+                    Loss = 30 * 0.25,
+                    OnepNetwork = onepNetwork
+                };
+                onepFiberTL1.OnepTopologicallinkMemberByUniMate = onepFiberTL2;
+                onepFiberTL2.OnepTopologicallinkMemberByUniMate = onepFiberTL1;
+
+                onepNetwork.OnepFibertls.Add(onepFiberTL1);
+                onepNetwork.OnepFibertls.Add(onepFiberTL2);
+
+                /////////
+
+                var onepTP1 = new OnepTerminationpoint()
+                {
+                    Name = "TP 01",
+                    Ptp = 1,
+                    Role = 2,
+                    OnepNetwork = onepNetwork
+                };
+                var onepTP2 = new OnepTerminationpoint()
+                {
+                    Name = "TP 02",
+                    Ptp = 1,
+                    Role = 2,
+                    OnepNetwork = onepNetwork
+                };
+
+                onepNetwork.OnepTerminationpoints.Add(onepTP1);
+                onepNetwork.OnepTerminationpoints.Add(onepTP2);
+
+                onepTP1.OnepTopologicallinksForAEndTP.Add(onepFiberTL1);
+                onepTP1.OnepTopologicallinksForZEndTP.Add(onepFiberTL2);
+                onepFiberTL1.OnepTerminationpointByAEndTP = onepTP1;
+                onepFiberTL2.OnepTerminationpointByAEndTP = onepTP2;
+
+                onepTP2.OnepTopologicallinksForAEndTP.Add(onepFiberTL2);
+                onepTP2.OnepTopologicallinksForZEndTP.Add(onepFiberTL1);
+                onepFiberTL1.OnepTerminationpointByZEndTP = onepTP2;
+                onepFiberTL2.OnepTerminationpointByZEndTP = onepTP1;
+
+                var onepAmpTP1 = new OnepAmptp()
+                {
+                    TargetGain = 2.1,
+                    OnepNetwork = onepNetwork,
+                    OnepTerminationpoint = onepTP1
+                };
+                onepTP1.OnepAmpRole = onepAmpTP1;
+                onepNetwork.OnepAmptps.Add(onepAmpTP1);
+
+                var onepAmpTP2 = new OnepAmptp()
+                {
+                    TargetGain = 1.9,
+                    OnepNetwork = onepNetwork,
+                    OnepTerminationpoint = onepTP2
+                };
+                onepTP2.OnepAmpRole = onepAmpTP2;
+                onepNetwork.OnepAmptps.Add(onepAmpTP2);
+
+                var onepValidationResult01 = new OnepValidationresult
+                {
+                    Status = 1,
+                    OnepNetwork = onepNetwork
+                };
+
+                var onepVP01_1 = new OnepValidochpath
+                {
+                    Name = "VP 01_1",
+                    OnepNetwork = onepNetwork,
+                    Pmd = 2.3,
+                    OnepValidationresult = onepValidationResult01
+                };
+                onepNetwork.OnepValidochpaths.Add(onepVP01_1);
+
+                var onepVP01_2 = new OnepValidochpath
+                {
+                    Name = "VP 01_2",
+                    OnepNetwork = onepNetwork,
+                    Pmd = 2.4,
+                    OnepValidationresult = onepValidationResult01
+                };
+                onepNetwork.OnepValidochpaths.Add(onepVP01_2);
+
+                onepValidationResult01.OnepValidochpaths.Add(onepVP01_1);
+                onepValidationResult01.OnepValidochpaths.Add(onepVP01_2);
+                onepNetwork.OnepValidationresults.Add(onepValidationResult01);
+
+                // Create another Validation result
+
+                var onepValidationResult02 = new OnepValidationresult
+                {
+                    Status = 1,
+                    OnepNetwork = onepNetwork
+                };
+                onepNetwork.OnepValidationresults.Add(onepValidationResult02);
+
+            }
+
+            return onepNetwork;
+        }
+
+        private void ResetLE_01(OnepNetwork onepNetwork)
+        {
+            // Network has 1 ValidationResult which has 2 VPs attached
+
+            // We want to try re-parenting VPs, i.e. Remove ValidationResult01 and Create ValidationResult02
+            // And move the existing VPs to the new ValidationResult
+
+            var onepValidationresult01 = onepNetwork.OnepValidationresults[0];
+            var oldVPs = onepValidationresult01.OnepValidochpaths;
+
+            //OnepValidationresult onepValidationresult02 = new OnepValidationresult
+            //{
+            //    OnepNetwork = onepNetwork,
+            //    OnepValidochpaths = oldVPs
+            //};
+            var onepValidationresult02 = onepNetwork.OnepValidationresults[1];
+            foreach (var vp in oldVPs)
+            {
+                onepValidationresult02.OnepValidochpaths.Add(vp);
+                vp.OnepValidationresult = onepValidationresult02;
+            }
+
+            onepNetwork.OnepValidationresults.Remove(onepValidationresult01);
+            onepValidationresult01.OnepNetwork = null;
+            onepValidationresult01.OnepValidochpaths.Clear();
+
+            onepNetwork.OnepValidationresults.Add(onepValidationresult02);
         }
 
         // PUT: api/Networks/5
@@ -226,6 +469,8 @@ namespace OnepMini.Controllers
 
                     await session.SaveOrUpdateAsync(onepNetwork).ConfigureAwait(false);
                     tx.Commit();
+                    session.Close();
+
                 }
                 catch (Exception ex)
                 {
@@ -233,6 +478,8 @@ namespace OnepMini.Controllers
 
                     Debug.WriteLine(ex.ToString());
                     tx.Rollback();
+                    session.Close();
+
                     throw;
                 }
             }
@@ -281,18 +528,23 @@ namespace OnepMini.Controllers
         [HttpPut("EditNetwork/{id}/EditVPs")]
         public async Task<object> EditVPs(long id, [FromBody] string value)
         {
+            // Get the network
             var session = _sessionFactory.OpenSession();
             var onepNetwork = session.Get<OnepNetwork>(id);
 
+            // LE Reset
+            // Get ValidationResult01's VP's and associate them with a new ValidationResult
+            EditNetworkEditVPs(onepNetwork);
+
+            // Finally save the network back
             using (var tx = session.BeginTransaction())
             {
                 try
                 {
-                    // Replace TP01 by new TP03, but move the same AmpRole object to TP03
-                    EditNetworkEditVPs(onepNetwork);
-
                     await session.SaveOrUpdateAsync(onepNetwork).ConfigureAwait(false);
                     tx.Commit();
+                    session.Close();
+
                 }
                 catch (Exception ex)
                 {
@@ -300,6 +552,8 @@ namespace OnepMini.Controllers
 
                     Debug.WriteLine(ex.ToString());
                     tx.Rollback();
+                    session.Close();
+
                     throw;
                 }
             }
@@ -315,22 +569,23 @@ namespace OnepMini.Controllers
             // And move the existing VPs to the new ValidationResult
 
             var onepValidationresult01 = onepNetwork.OnepValidationresults[0];
+            var oldVPs = onepValidationresult01.OnepValidochpaths;
+            //onepValidationresult01.OnepValidochpaths.Clear();
 
-            OnepValidationresult onepValidationresult02 = new OnepValidationresult
+            //OnepValidationresult onepValidationresult02 = new OnepValidationresult
+            //{
+            //    OnepNetwork = onepNetwork,
+            //    OnepValidochpaths = oldVPs
+            //};
+            var onepValidationresult02 = onepNetwork.OnepValidationresults[1];
+            foreach (var vp in oldVPs)
             {
-                OnepNetwork = onepNetwork,
-                OnepValidochpaths = onepValidationresult01.OnepValidochpaths
-            };
-
-            foreach (var vp in onepNetwork.OnepValidochpaths)
-            {
+                onepValidationresult02.OnepValidochpaths.Add(vp);
                 vp.OnepValidationresult = onepValidationresult02;
             }
 
-            onepValidationresult01.OnepValidochpaths.Clear();
-
             onepNetwork.OnepValidationresults.Remove(onepValidationresult01);
-            onepNetwork.OnepValidationresults.Add(onepValidationresult02);
+            //onepNetwork.OnepValidationresults.Add(onepValidationresult02);
         }
 
         [HttpPut("EditNetwork/{id}/EditVPs2")]
@@ -355,6 +610,8 @@ namespace OnepMini.Controllers
 
                     await session2.SaveOrUpdateAsync(onepNetwork).ConfigureAwait(false);
                     tx.Commit();
+                    session2.Close();
+
                 }
                 catch (Exception ex)
                 {
@@ -362,6 +619,8 @@ namespace OnepMini.Controllers
 
                     Debug.WriteLine(ex.ToString());
                     tx.Rollback();
+                    session2.Close();
+
                     throw;
                 }
             }
@@ -392,6 +651,8 @@ namespace OnepMini.Controllers
 
                     await session2.SaveOrUpdateAsync(onepNetwork).ConfigureAwait(false);
                     tx.Commit();
+                    session2.Close();
+
                 }
                 catch (Exception ex)
                 {
@@ -399,6 +660,8 @@ namespace OnepMini.Controllers
 
                     Debug.WriteLine(ex.ToString());
                     tx.Rollback();
+                    session2.Close();
+
                     throw;
                 }
             }
@@ -413,9 +676,23 @@ namespace OnepMini.Controllers
             var session = _sessionFactory.OpenSession();
             using (var tx = session.BeginTransaction())
             {
-                var onepNetwork = session.Get<OnepNetwork>(id);
-                session.Delete(onepNetwork);
-                tx.Commit();
+                try
+                {
+                    var onepNetwork = session.Get<OnepNetwork>(id);
+                    session.Delete(onepNetwork);
+                    tx.Commit();
+                    session.Close();
+                }
+                catch (Exception ex)
+                {
+                    Debugger.Break();
+
+                    Debug.WriteLine(ex.ToString());
+                    tx.Rollback();
+                    session.Close();
+
+                    throw;
+                }
             }
         }
     }
--- OnepMini/OnepMini/ReadMe.txt
+++ OnepMini/OnepMini/ReadMe.txt
@@ -17,3 +17,18 @@ OnePlanner Mini version
 
 (#) Added APIs in NetworksController - Create new OnepNetwork, Delete, ChangeTerminationPoint
 (#) Add VP and ValidationResult
+(#) Reproduce Object Deleted exception
+		CreateNetworkForLEReset_01 + ResetLE_01
+            // This sequence of changes results in the following exception:
+            // NHibernate.ObjectDeletedException: deleted object would be re-saved by cascade (remove deleted object from associations)[OnepMini.OrmNhib.BusinessObjects.OnepValidochpath#163840]
+
+            // Summary:
+            // We already have VR01 and VR02 in the network
+            // During LE reset, move VPs from under VR01 to VR02
+            // And then we remove VR01 from network
+
+			// PS: Dont change the reference of collections, for eg, dont do VR02.VPs = VR01.VPs .. that will result in another exception
+				// Don't change the reference to a collection with cascade=“all-delete-orphan”
+
+			// The other thing to note is that if we dont remove the old VR from network, everything works fine
+				.. reparenting happens with no issues, except of course that the old VR is still in the network
